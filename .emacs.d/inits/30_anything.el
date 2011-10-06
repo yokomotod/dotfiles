aa895d9c00b3607d46237c1c99e99af308d91804
Daisuke Yokomoto <yokomotod@gmail.com>
2011-10-06 21:28:18 +0900

    fix emacs anything-filelist+ key bind

--- .emacs.d/inits/30_anything.el
+++ .emacs.d/inits/30_anything.el
@@ -2,5 +2,5 @@
 (setq anything-c-filelist-file-name "~/.emacs.d/all.filelist")
 ;; (setq anything-grep-candidates-fast-directory-regexp "^/tmp")
 
-(global-set-key "\C-xb" 'anything)
+(global-set-key "\C-xb" 'anything-filelist+)
 
