cf83109a2eceab1afcefed411e8944dacb94a17d
yokomotod <yokomotod@gmail.com>
2014-04-26 21:30:28 +0900

    add .chef

---
+++ .chef/knife.rb
@@ -0,0 +1,8 @@
+log_level                :info
+log_location             STDOUT
+node_name                'yokomotod'
+client_key               '/Users/yokomoto/.chef/yokomotod.pem'
+validation_client_name   'chef-validator'
+validation_key           '/etc/chef-server/chef-validator.pem'
+chef_server_url          'https://mars.local:443'
+syntax_check_cache_path  '/Users/yokomoto/.chef/syntax_check_cache'
