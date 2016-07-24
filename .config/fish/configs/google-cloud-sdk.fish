6609c434837a81bdf81c6541df7bbf4ab09df904
yokomotod <yokomotod@gmail.com>
2016-07-24 20:07:34 +0900

    Add google-cloud-sdk.fish

---
+++ .config/fish/configs/google-cloud-sdk.fish
@@ -0,0 +1,2 @@
+set fish_user_paths /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin
+set -x MANPATH /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/help/man /usr/local/share/man /usr/share/man /opt/x11/share/man
