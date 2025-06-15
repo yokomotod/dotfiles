c40144db2d4815a0dfeffb9149cac97b7a4ad787
yokomotod <yokomotod@gmail.com>
2025-06-15 17:58:02 +0900

    nix: git config

--- home.nix
+++ home.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }:
 
 {
+  home.sessionPath = [ "$HOME/.local/bin" ];
+
   home.packages = with pkgs; [
     ripgrep
     nodejs
@@ -20,6 +22,13 @@
       keychain -q $HOME/.ssh/id_ed25519
       source $HOME/.keychain/$HOSTNAME-sh
     '';
+  };
+
+  xdg.configFile."git/config".source = ./.config/git/config;
+  xdg.configFile."git/ignore".source = ./.config/git/ignore;
+  home.file.".local/bin/diff-highlight" = {
+    source = "${pkgs.git}/share/git/contrib/diff-highlight/diff-highlight";
+    executable = true;
   };
 
   home.stateVersion = "24.11";
